3dbf5224b7f22408342ad5a415921e26283fe1dd
Huu_Tu <57584396+Huu-tu@users.noreply.github.com>
2025-08-25 06:57:52 +0700

    setup taiwindcss and daisy

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './**/*.py',
+  ],
+  theme: {
+  fontFamily: {
+      body: [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "system-ui",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+      sans: [
+        "Inter",
+        "ui-sans-serif",
+        "system-ui",
+        "-apple-system",
+        "system-ui",
+        "Segoe UI",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial",
+        "Noto Sans",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
